2201432cf278a7a2ed4824ab34f62292a71addc1
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 18:54:43 +0700

    Update 10.py

--- 10.py
+++ 10.py
@@ -3,9 +3,9 @@ lst2 = list(map(int, input().split()))
 nums = list(map(int, input().split()))
 start = nums[0]
 end = nums[1]
-fragment = lst1[start : end + 1]
+fragment = lst1[start:end + 1]
 fragment.reverse()
 lst2 = lst2 + fragment
-del lst1[start : end + 1]
+del lst1[start:end + 1]
 print(lst1)
 print(lst2)
